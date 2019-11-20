819f4981ee5a6f8189326ab17bf052ffe75796b9
erfanhss <57976517+erfanhss@users.noreply.github.com>
2019-11-20 00:04:00 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,66 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import pickle
+import sys
+import os
+from keras import backend as K
+
+def load_batch(fpath, label_key='labels'):
+    """Internal utility for parsing CIFAR data.
+    # Arguments
+        fpath: path the file to parse.
+        label_key: key for label data in the retrieve
+            dictionary.
+    # Returns
+        A tuple `(data, labels)`.
+    """
+    with open(fpath, 'rb') as f:
+        if sys.version_info < (3,):
+            d = pickle.load(f)
+        else:
+            d = pickle.load(f, encoding='bytes')
+            # decode utf8
+            d_decoded = {}
+            for k, v in d.items():
+                d_decoded[k.decode('utf8')] = v
+            d = d_decoded
+    data = d['data']
+    labels = d[label_key]
+
+    data = data.reshape(data.shape[0], 3, 32, 32)
+    return data, labels
+
+
+def load_data():
+    """Loads CIFAR10 dataset.
+    # Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+    """
+    path = 'cifar-10-batches-py'
+
+    num_train_samples = 50000
+
+    x_train_local = np.empty((num_train_samples, 3, 32, 32), dtype='uint8')
+    y_train_local = np.empty((num_train_samples,), dtype='uint8')
+
+    for i in range(1, 6):
+        fpath = os.path.join(path, 'data_batch_' + str(i))
+        (x_train_local[(i - 1) * 10000: i * 10000, :, :, :],
+         y_train_local[(i - 1) * 10000: i * 10000]) = load_batch(fpath)
+
+    fpath = os.path.join(path, 'test_batch')
+    x_test_local, y_test_local = load_batch(fpath)
+
+    y_train_local = np.reshape(y_train_local, (len(y_train_local), 1))
+    y_test_local = np.reshape(y_test_local, (len(y_test_local), 1))
+
+    if K.image_data_format() == 'channels_last':
+        x_train_local = x_train_local.transpose(0, 2, 3, 1)
+        x_test_local = x_test_local.transpose(0, 2, 3, 1)
+
+    return (x_train_local, y_train_local), (x_test_local, y_test_local)
+
 
 def classifier_model(input_image, image_labels):
     with tf.variable_scope("classifier_vars") as scope:
@@ -142,7 +202,7 @@ def perform_one_subset(train_x, train_y, test_x, test_y, sub_set_id, batch_size)
 
 
 print('Preparing data')
-(train_x, train_y), (test_x, test_y) = tf.keras.datasets.cifar10.load_data()
+(train_x, train_y), (test_x, test_y) = load_data()
 train_x = train_x / 255.
 test_x = test_x / 255.
 MIE_vec = []
